149f5ffdcfeaf67270b90f2456e5bb23e40b353f
sergeykorolov <korolovsergio@mail.ru>
2020-05-21 18:22:17 +0300

    Updates

---
+++ precache-manifest.7f18b4239da69b7b72cf03e291180cb8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "158c25d6e8af3147e60c77026d42bec8",
+    "url": "/PersonalArea/index.html"
+  },
+  {
+    "revision": "05920ee7d1433bbfc19c",
+    "url": "/PersonalArea/static/css/main.79add735.chunk.css"
+  },
+  {
+    "revision": "47147bb24181eb1c497c",
+    "url": "/PersonalArea/static/js/2.34acc5a0.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/PersonalArea/static/js/2.34acc5a0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "05920ee7d1433bbfc19c",
+    "url": "/PersonalArea/static/js/main.691f357e.chunk.js"
+  },
+  {
+    "revision": "f7a4c3b9c11c01a95f66",
+    "url": "/PersonalArea/static/js/runtime-main.d36c5919.js"
+  }
+]);
